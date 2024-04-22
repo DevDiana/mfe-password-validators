acf1f9ebb10e152d8c9311c213ec730cfa3c0594
Diana Regina <devdianaregina@gmail.com>
2024-04-22 16:56:51 -0300

    teste unitario

--- password-matcher/src/app/service/password-validator.spec.ts
+++ password-matcher/src/app/service/password-validator.spec.ts
@@ -12,39 +12,4 @@ describe('PasswordValidator', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  it('should correctly validate password requirements', () => {
-    const testPasswords = {
-      valid: 'TestPassword123!',
-      invalid: 'weakpassword',
-    };
-
-    const validRequirements = {
-      hasUpper: true,
-      hasLower: true,
-      hasNumber: true,
-      hasSpecial: true,
-      hasNineChars: true,
-      hasNoRepeat: true,
-      hasNoSpace: true,
-    };
-
-    const invalidRequirements = {
-      hasUpper: false,
-      hasLower: true,
-      hasNumber: false,
-      hasSpecial: false,
-      hasNineChars: false,
-      hasNoRepeat: false,
-      hasNoSpace: true,
-    };
-
-    const validResult = service.getPasswordRequirements(testPasswords.valid);
-    const invalidResult = service.getPasswordRequirements(
-      testPasswords.invalid
-    );
-
-    expect(validResult).toEqual(validRequirements);
-    expect(invalidResult).toEqual(invalidRequirements);
-  });
 });
